641faa7a3225d6599e06f6e513a7a6a0ad9247f4
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-11-14 13:21:59 +0900

    Update

--- sparseir-capi/examples/spir_uhat_get_default_matsus.jl
+++ sparseir-capi/examples/spir_uhat_get_default_matsus.jl
@@ -73,14 +73,110 @@ function spir_basis_new(
 end
 
 # %%
+mutable struct spir_funcs end
+
+function spir_funcs_get_size(funcs::Ptr{spir_funcs})
+    sz = Ref{Int32}(0)
+    status = ccall(
+        (:spir_funcs_get_size, libpath),
+        Int32,
+        (Ptr{spir_funcs}, Ptr{Int32}),
+        funcs, sz,
+    )
+    return sz[]
+end
+
+# %%
+function spir_basis_get_uhat_full(basis::Ptr{spir_basis})
+    status = Ref{Int32}(0)
+    uhat_full = ccall(
+        (:spir_basis_get_uhat_full, libpath),
+        Ptr{spir_funcs},
+        (Ptr{spir_basis}, Ref{Int32}),
+        basis, status)
+    if uhat_full == C_NULL
+        error("Failed to create uhat: status = $(status[])")
+    end
+    return uhat_full
+end
+
+function spir_basis_get_uhat(basis::Ptr{spir_basis})
+    status = Ref{Int32}(0)
+    uhat = ccall(
+        (:spir_basis_get_uhat, libpath),
+        Ptr{spir_funcs},
+        (Ptr{spir_basis}, Ref{Int32}),
+        basis, status)
+    if uhat == C_NULL
+        error("Failed to create uhat: status = $(status[])")
+    end
+    return uhat
+end
+
+# %%
+function spir_uhat_get_default_matsus(
+    uhat::Ptr{spir_funcs},
+    l::Integer,
+    positive_only::Bool,
+    mitigate::Bool
+)
+    # Pre-allocate buffer - allocate enough space for worst case (l points, possibly more with mitigation)
+    max_points = l + 10  # Allocate extra space to be safe
+    points_buffer = Vector{Int64}()
+    n_points_returned = Ref{Int32}(0)
+    status = ccall(
+        (:spir_uhat_get_default_matsus, libpath),
+        Int32,
+        (Ptr{spir_funcs}, Int32, Bool, Bool, Ptr{Int64}, Ptr{Int32}),
+        uhat, Cint(l), positive_only, mitigate, pointer(points_buffer), n_points_returned
+    )
+    if status != 0
+        error("Failed to get default Matsubara frequencies: status = $status")
+    end
+    # Return only the actual points that were written
+    unsafe_wrap(Vector{Int64}, pointer(points_buffer), n_points_returned[])
+end
+
+# %%
 let
     kernel = kernel_logistic_new(10.0)
     basis = spir_basis_new(1, 10.0, 1.0, 1e-6, kernel)
+    uhat_full = spir_basis_get_uhat_full(basis)
+    uhat = spir_basis_get_uhat(basis)
+    uhat_size = spir_funcs_get_size(uhat)
+    uhat_full_size = spir_funcs_get_size(uhat_full)
+    @assert uhat_size == 10
+    @assert uhat_full_size >= uhat_size
+
+    let
+        positive_only = true
+        mitigate = false
+        default_matsus_positive_only = spir_uhat_get_default_matsus(
+            uhat_full, 10, positive_only, mitigate
+        )
+        @assert default_matsus_positive_only == [1, 3, 5, 7, 23]
+    end
+
+    let
+        positive_only = false
+        mitigate = false
+        default_matsus_positive_only = spir_uhat_get_default_matsus(
+            uhat_full, 10, positive_only, mitigate
+        )
+        @assert default_matsus_positive_only == [
+            -23, -7, -5, -3, -1, 1, 3, 5, 7, 23
+        ]
+    end
 end
 
 # %%
 let
     kernel = SparseIR.LogisticKernel(10.0)
     basis = SparseIR.FiniteTempBasis{SparseIR.Fermionic}(10.0, 1.0, 1e-6)
-    basis.uhat_full
+    basis.uhat
+    SparseIR.default_matsubara_sampling_points(
+        basis.uhat_full, 10; fence=false, positive_only=false
+    )
 end
+
+# %%
